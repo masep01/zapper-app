751d3f96d100813a414740aa73744c31c0ccf3c7
thereal12martin <mlopezaranburu@gmail.com>
2024-04-28 22:07:19 +0200

    LogIn + SignUp done, començar API calls

--- zapper/App/Screens/SignUpScreen.js
+++ zapper/App/Screens/SignUpScreen.js
@@ -1,68 +1,112 @@
 import React, {useState} from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Button, ScrollView} from 'react-native';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const SignupScreen = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [conf_passwd, setConfPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
+  const [instagram, setInstagram] = useState('');
+  const [twitter, setTwitter] = useState('');
   const [birthDate, setBirthDate] = useState('');
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
 
   const handleSignUp = () => {
     // Aquí puedes implementar la lógica para autenticar al usuario
+    const dateObj = new Date(birthDate);
+    const dia = dateObj.getDate();
+    const mes = dateObj.getMonth() + 1;
+    const año = dateObj.getFullYear();
+    const newDate = `${dia}-${mes}-${año}`;
+
     console.log('Username:', username);
-    console.log('Name:', name);
-    console.log('Password:', password);
     console.log('Email:', email);
-    console.log('Birth Date:', birthDate);
+    console.log('Birth Date:', newDate);
+    console.log('Instagram:', instagram);
+    console.log('Twitter:', twitter);
+    console.log('Password:', password);
+    console.log('Good PSWD', password == conf_passwd);
+
+    
 
 
     // Por ejemplo, puedes enviar estos datos a un backend
   };
 
+  const showDatePicker = () =>{
+    setDatePickerVisibility(true);
+  }
+
+  const hideDatePicker = () =>{
+    setDatePickerVisibility(false);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Sign Up</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        onChangeText={(text) => setUsername(text)}
-        value={username}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        onChangeText={(text) => setName(text)}
-        value={name}
-      />
-      <RNDateTimePicker
-      style={styles.input}
-      placeholder="Birth Date" 
-      mode="date" 
-      display = "default"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-        secureTextEntry
-      />
-      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-        <Text style={styles.buttonText}>Sign Up</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView contentContainerStyle={styles.ScrollViewContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+        />
+        <TouchableOpacity style={styles.buttonBD} onPress={showDatePicker}>
+          <Text style={styles.buttonText}>Select Birth Date</Text> 
+        </TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder="Instagram"
+          onChangeText={(text) => setInstagram(text)}
+          value={instagram}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Twitter"
+          onChangeText={(text) => setTwitter(text)}
+          value={twitter}
+        />
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode='date'
+          onConfirm={(datePicked) => setBirthDate(datePicked)}
+          onCancel={hideDatePicker}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Repeat your password"
+          onChangeText={(text) => setConfPassword(text)}
+          value={conf_passwd}
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollViewContainer: {
+    flexGrow: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -78,13 +122,24 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ccc',
     marginBottom: 10,
+    marginTop: 10,
     padding: 10,
+  },
+  datePicker: {
+    
   },
   button: {
     backgroundColor: 'blue',
     padding: 10,
     borderRadius: 5,
     width: '80%',
+    alignItems: 'center',
+  },
+  buttonBD: {
+    backgroundColor: '#3378ed',
+    padding: 10,
+    borderRadius: 5,
+    width: '50%',
     alignItems: 'center',
   },
   buttonText: {
